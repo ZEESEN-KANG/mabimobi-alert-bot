698028fcbbf1ef359c80e6bb5d98196ed041644c
ZEESEN <39490993+ZEESEN-KANG@users.noreply.github.com>
2025-08-14 16:43:48 +0900

    Delete module directory

--- module/logger.py
+++
@@ -1,8 +0,0 @@
-# module/logger.py
-import logging
-
-def setup_logger() -> None:
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s"
-    )
